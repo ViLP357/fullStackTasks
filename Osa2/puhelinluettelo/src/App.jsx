0e3a19b24d39d38f291de98f9a410945ef180c91
ViLP357 <vilipi2009@gmail.com>
2024-12-23 20:38:05 +0200

    puhelinluettelo 2.11 step6

--- Osa2/puhelinluettelo/src/App.jsx
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react'
+import axios from 'axios'
+
 
 const Filter = ({newFilter, handleFilterChange}) => {
   return (
@@ -43,16 +45,20 @@ const PersonsToShow = ({ persons, filterInUse }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3002/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
 
   const addPerson = (event) => {
     event.preventDefault()
