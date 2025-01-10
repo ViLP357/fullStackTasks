ef92d1901c4b3a1e1a41cc06a320c39ad41a63a7
ViLP357 <vilipi2009@gmail.com>
2025-01-10 17:27:25 +0200

    puhelinluettelo kopioitu ja muokattu

--- Osa3/puhelinluetteloFullStack/src/services/person.js
+++ Osa3/puhelinluetteloFullStack/src/services/person.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons' //step12 
-//const baseUrl = 'http://localhost:3001/api/persons' //with backend
+//const baseUrl = 'http://localhost:3001/persons' //step12 
+const baseUrl = 'http://localhost:3001/api/persons' //with backend
 
 const getAll = () => {
   const request = axios.get(baseUrl)
