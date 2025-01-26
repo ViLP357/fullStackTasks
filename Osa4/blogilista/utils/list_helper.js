11902d72db0e4939469c1e1fd8598beae2cb52f3
ViLP357 <vilipi2009@gmail.com>
2025-01-26 12:40:33 +0200

    t.4.5

--- Osa4/blogilista/utils/list_helper.js
+++ Osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,33 @@
 const dummy = (blogs) => {
     return 1
   }
+
+const totalLikes = (blogs) => {
+    //console.log(blogs.length)
+    if (blogs.length === 0) {
+        return 0
+    }
+    else {
+      const summa = 0
+      const result = blogs.reduce( (s, value) => s +  value.likes, summa)
+      //onsole.log("tulos", result)
+      return result
+    }
+}
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0
+  }
+  const likes = blogs.map(blog => blog.likes)
+  //console.log(likes)
+  suurin =  Math.max(...likes)
+  const mostLikedBlogs = blogs.filter(blog => blog.likes === suurin)
+  //console.log(mostLikedBlogs)
+  return mostLikedBlogs[0]
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    mostLikes
   }
