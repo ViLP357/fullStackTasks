21c88cb1d54ab767abd3babc7a84be7e82e134ad
ViLP357 <vilipi2009@gmail.com>
2025-02-12 16:18:17 +0200

    t.5.15

--- Osa5/FullBlogilista/bloglist-frontend/src/components/Blog.test.jsx
+++ Osa5/FullBlogilista/bloglist-frontend/src/components/Blog.test.jsx
@@ -31,7 +31,8 @@ test('view button shows more information', async () => {
     url: "nofing.com",
     user: {
         id: 12345
-    }
+    },
+    likes: 4
   }
   const users = [{
     name: "tester",
@@ -41,16 +42,54 @@ test('view button shows more information', async () => {
 
   render(<Blog blog={blog} users={users} userWithToken={users[0]} />)
 
-  //const mockHandler = vi.fn()
-
   const user = userEvent.setup()
   const button = screen.getByText("view")
   await user.click(button)
-  //console.log("btn found")
 
-  //expect(mockHandler.mock.calls).toHaveLength(1)
-
-  const element = screen.getByText(/Component testing/i)
+  const title = screen.getByText(/Component testing/i)
   screen.debug()
-  expect(element).toBeDefined()
+  expect(title).toBeDefined()
+
+  const author = screen.getByText(/Component/i)
+  expect(author).toBeDefined()
+
+  const url = screen.getByText(/nofing.com/i)
+  expect(url).toBeDefined()
+
+  const likes = screen.getByText(/4/i)
+  expect(likes).toBeDefined()
+
+  const kayttaja = screen.getByText(/tester/i)
+  expect(kayttaja).toBeDefined()
+})
+
+test("liking twice adds two likes", async () => {
+  const blog = {
+    title: "Component testing",
+    author: "Component",
+    url: "nofing.com",
+    user: {
+        id: 12345
+    },
+    likes: 4
+  }
+  const users = [{
+    name: "tester",
+    username: "test",
+    id: 12345
+  }]
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} users={users} userWithToken={users[0]} likeBlog={mockHandler}/>)
+  const user = userEvent.setup()
+
+  const buttonView = screen.getByText("view")
+  await user.click(buttonView)
+
+  const buttonLike = screen.getByText("like")
+  await user.click(buttonLike)
+  await user.click(buttonLike)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
 })
