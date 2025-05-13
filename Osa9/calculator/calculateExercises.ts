ad4a1d65a38ebf386a39b0b0110fbc864ef5c21f
ViLP357 <vilipi2009@gmail.com>
2025-05-13 16:56:11 +0300

    t. 9.3

--- Osa9/calculator/calculateExercises.ts
+++ Osa9/calculator/calculateExercises.ts
@@ -7,6 +7,35 @@ interface ExerciseObject {
     target: number,
     avarage: number
 }
+interface arguments {
+    target: number,
+    list: number[]
+}
+
+const parseArguments = (args: string[]) : arguments => {
+    if (args.length < 4){
+        throw new Error("Not enough arguments");
+    }
+    let trg = args[2];
+    let nums : number[] = [];
+    //console.log("Arvot:")
+    for (let i = 3; i<args.length; i++) {
+        if (!isNaN(Number(args[i]))) {
+            nums.push(Number(args[i]));
+            //console.log(Number(args[i]));
+        } else {
+            throw new Error("Provided value was not a number");
+        }
+    }
+
+    if (!isNaN(Number(args[2]))) {
+        return {target: Number(trg),
+            list: nums
+        };
+    } else {
+        throw new Error("Provided value was not a numbr");
+    }
+}
 
 const CalculateExercises = (list : number[], target: number) : ExerciseObject => {
     let avrg: number;
@@ -31,11 +60,12 @@ const CalculateExercises = (list : number[], target: number) : ExerciseObject =>
         suc = false;
         if (avrg + 0.5 >= target) {
             rtng = 2;
-            descText = "Not bad. Try harder next time"
+            descText = "Not bad. Try harder next time";
         }
-        else 
+        else {
             rtng = 1;
-            descText = "Not very good... Did you even try?"
+            descText = "Not very good... Did you even try?";
+        }
     }
     return {
         periodLength: list.length,
@@ -48,6 +78,8 @@ const CalculateExercises = (list : number[], target: number) : ExerciseObject =>
     }
 }
 
-let target : number = 2;
-let list : number[] = [3, 0, 2, 4.5, 0, 3, 1];
+//let target : number = 2;
+//let list : number[] = [3, 0, 2, 4.5, 0, 3, 1];
+
+const {target, list} = parseArguments(process.argv);
 console.log(CalculateExercises(list, target));
