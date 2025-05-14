e475da118f5e45856c326680bbc9688f3ba7937b
ViLP357 <vilipi2009@gmail.com>
2025-05-14 15:54:56 +0300

    t.9.5

--- Osa9/calculator/index.ts
+++ Osa9/calculator/index.ts
@@ -1,16 +1,30 @@
 //const express = require("express");
 import express from 'express';
+//const qs = require('qs');
+
+
 //import { calculateBmi } from './bmiCalculator';
 //import { calculateBmi } from './bmiCalculator';
 const { calculateBmi } = require("./bmiCalculator");
 const app = express();
 
+
+
 app.get('/hello', (_req, res) => {
     res.send("Hello Full Stack!");
 });
-app.get('/bmi', (_req, res) => {
-    const a : number = Number(190);
-    const b : number = Number(80);
+app.get('/bmi', (req, res) => {
+
+    
+    //console.log(req.url);
+    //const [, params] = req.url.split('?');
+//
+    //const parsedParams = qs.parse(params);
+    //console.log(parsedParams);
+    
+    const a : number = Number(req.query.height) || 190;
+    const b : number = Number(req.query.weight) || 89;
+
 
     const result : string = calculateBmi(a,b);
     res.send({weight: a,
