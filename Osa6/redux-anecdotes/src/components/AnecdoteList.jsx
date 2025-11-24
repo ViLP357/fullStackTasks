47628ddc4e140d33ac12725da9e916720ba3a508
ViLP357 <vilipi2009@gmail.com>
2025-11-24 16:33:15 +0200

    t.6.11

--- Osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,14 +3,14 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 const AnecdoteList = () => {
 
     const anecdotes = useSelector(state => {
-        console.log(state.filter)
+        //console.log(state.filter)
 
         return state.anecdotes.filter(a =>a.content.includes(state.filter))
 
     })
     const dispatch = useDispatch()
     const vote = id => {
-        console.log(id)
+        //console.log(id)
         dispatch(voteAnecdote(id))
     }
     return (
--- Osa6/redux-anecdotes/src/components/Filter.jsx
+++ Osa6/redux-anecdotes/src/components/Filter.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux"
-import { filterChange } from "../reducers/filterReducer"
+//import { filterChange } from "../reducers/filterReducer"
+import { filterChange } from '../reducers/filterReducer'
 
 
 const Filter = () => {
